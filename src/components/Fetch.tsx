49d10dc80259f5c3ccc5077c4371c0b7d08224a6
Rafa <rafa_zkeleton@hotmail.com>
2021-07-15 12:54:00 -0500

    proxy para fetch

--- src/components/Fetch.tsx
+++ src/components/Fetch.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default async function Fetch(apiRoute:any){
-    const route = 'https://sniiv.conavi.gob.mx/SNIIV.svc/'
+    const route = 'https://sniiv-cors.herokuapp.com/https://sniiv.conavi.gob.mx/SNIIV.svc/'
 
     return fetch(route+apiRoute,{
         method: 'GET',
