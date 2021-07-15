0fb9576c677fff50ac4bd8abd33310a5d98a157e
Rafa <rafa_zkeleton@hotmail.com>
2021-07-15 11:34:14 -0500

    Proxy

--- src/components/Fetch.tsx
+++ src/components/Fetch.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default async function Fetch(apiRoute:any){
-    const route = '/SNIIV.svc/'
+    const route = 'https://sniiv.conavi.gob.mx/SNIIV.svc/'
 
     return fetch(route+apiRoute,{
         method: 'GET',
